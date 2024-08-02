932444f9ee750bb44f1db19644194a159f324c26
Carlos01333R <116594383+Carlos01333R@users.noreply.github.com>
2024-08-02 12:17:27 -0500

    Update logo.jsx

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -6,7 +6,7 @@ export default function Logo() {
             WebkitMaskImage: 'linear-gradient(black 90%, transparent)',
             
         }}
-        className='flex justify-center items-center w-full   h-[450px]'>
+        className='flex justify-center items-center w-full   md:h-[450px]'>
         <img src="https://e0.pxfuel.com/wallpapers/653/886/desktop-wallpaper-dragon-ball-z-minimalist-dragon-ball-z-black.jpg" alt="logo"  />
         </section>
     </>)
